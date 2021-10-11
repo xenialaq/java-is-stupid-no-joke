629e0059a5bc748818dd6188a6cfb609a5866cd3
Alex <moodspace@outlook.com>
2021-10-11 15:49:06 -0500

    Create method-1.js

---
+++ leetcode.com/problems/valid-anagram/method-1.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    const sMap = new Map();
+    let i = 0;
+    let cp;
+    while (s.codePointAt(i++) !== undefined) {
+        cp = s.codePointAt(i-1)
+        sMap.set(cp, sMap.has(cp) ? (sMap.get(cp) + 1) : 1);
+    }
+    const tMap = new Map();
+    i = 0;
+    cp = undefined;
+    while (t.codePointAt(i++) !== undefined) {
+        cp = t.codePointAt(i-1)
+        tMap.set(cp, tMap.has(cp) ? (tMap.get(cp) + 1) : 1);
+    }
+    if (sMap.size !== tMap.size) {
+        return false;
+    }
+    for (const scp of sMap.keys()) {
+        if (sMap.get(scp) !== tMap.get(scp)) {
+            return false;
+        }
+    }
+    return true;
+};
